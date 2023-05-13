7fd6b87e0a76523ec60e11c13bb40e2a29b06d4f
lvh20895 <levanhau12321@gmail.com>
2023-05-13 15:01:58 +0700

    setup vercel

--- socket.js
+++ socket.js
@@ -5,13 +5,18 @@ const socketServer = http => {
 		cors: { origin: "*" }
 	});
 
+	let number = 0;
 	io.on("connection", socket => {
+		// socket.emit: chỉ gửi cho 1 client kết nối đến server
+		// io.emit: gửi cho tất cả client đang kết nối đến server
 		io.emit("fe-connect", socket.id);
 
 		socket.on("client to server", data => {
-			io.emit("server to client", data);
+			number++;
+			io.emit("server to client", number);
 		});
 
+		// lắng nghe 1 client thoát server
 		socket.on("disconnect", reason => {
 			io.emit("fe-dis", socket.id);
 		});
